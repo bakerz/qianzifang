645cda51481bfff800191c577a530f4cff2c9e55
bakerz <de1719@163.com>
2015-09-26 14:58:51 +0800

    add layout file

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,35 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '仟姿纺' });
+  res.render('index', {title: '仟姿纺'});
 });
 
-router.get('/', function(req, res, next) {
+router.get('/about', function(req, res, next) {
+	res.render('about', {title: ''});
+});
 
+router.get('/join', function(req, res, next) {
+	res.render('join', {title: ''});
+});
+
+router.get('/business', function(req, res, next) {
+	res.render('business', {title: ''});
+});
+
+router.get('/train', function(req, res, next) {
+	res.render('train', {title: ''});
+});
+
+router.get('/product', function(req, res, next) {
+	res.render('product', {title: ''});
+});
+
+router.get('/contact', function(req, res, next) {
+	res.render('contact', {title: ''});
+});
+
+router.get('/staff', function(req, res, next) {
+	res.render('staff', {title: ''});
 });
 
 module.exports = router;
