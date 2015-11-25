e29125a04d5b42215fe77bdcb323b47ce78f08bc
bakerz <de1719@163.com>
2015-11-25 10:57:13 +0800

    modified product data style

--- public/javascripts/getProduct.js
+++ public/javascripts/getProduct.js
@@ -1,17 +1,32 @@
 $(function() {
 	$('.category li').on('click', function() {
-		var clo_type = $(this).data('type');
+		var CLO_TYPE = $(this).data('type');
 		
-		$.ajax({
-			url: '/product/'+clo_type,
-			type: 'get',
-			dataType: 'json',
-			success: function(data) {
-				console.log(data);
-			},
-			error: function(data) {
-				console.log(data);
-			}
-		})
+		getProduct(CLO_TYPE);
 	});
+	
+	//getProduct('bra')
 })
+
+function getProduct(type) {
+	var SRC = '/images/product/'+ type +'/';
+	
+	$.ajax({
+		url: '/product/'+type,
+		type: 'get',
+		dataType: 'json',
+		success: function(data) {
+			var data = data.datas,
+				len = data.length,
+				html = '';
+			for(var i = 0; i < len; i++) {
+				html += '<div class="col-md-4 col-lg-3 pro-item">' 
+					+ '<img src="'+ SRC + data[i].link +'"></div>';
+			}
+			$('.pro-box-con').html(html);
+		},
+		error: function(data) {
+			console.log(data);
+		}
+	})
+}
--- routes/getProduct.js
+++ routes/getProduct.js
@@ -2,7 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/bra', function(req, res) {
-	res.json({'success': 1});
+	res.json({
+		'success': 1,
+		'datas': [
+			{'link': '1.jpg'},
+			{'link': '2.jpg'},
+			{'link': '3.jpg'},
+			{'link': '4.jpg'},
+			{'link': '5.jpg'},
+			{'link': '6.jpg'},
+			{'link': '7.jpg'},
+			{'link': '8.jpg'},
+			{'link': '9.jpg'},
+			{'link': '10.jpg'},
+			{'link': '11.jpg'},
+			{'link': '12.jpg'}
+		]
+	});
 });
 
 router.get('/underpants', function(req, res) {
